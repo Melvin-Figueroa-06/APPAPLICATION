8a39232567a061aee4ace2e3585cb950d5a3b214
Melvin Figueroa <melproducc@gmail.com>
2019-07-04 20:06:38 -0400

    mapas

---
+++ app/src/main/java/com/example/app_aplication/Iniciar_Sesion.java
@@ -0,0 +1,13 @@
+package com.example.app_aplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Iniciar_Sesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iniciar__sesion);
+    }
+}
---
+++ app/src/main/java/com/example/app_aplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.app_aplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        loadComponents();
+    }
+
+    private void loadComponents() {
+        Button button_iniciar = findViewById(R.id.button_iniciar);
+        button_iniciar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_iniciar:{
+                Intent iniciar_sesion = new Intent(this,Iniciar_Sesion.class);
+                startActivity(iniciar_sesion);
+                break;
+            }
+        }
+    }
+}
